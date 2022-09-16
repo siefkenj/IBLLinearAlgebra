ef631e1170713d496d4c14e263be065b418e92f1
Jason Siefken <siefkenj@gmail.com>
2022-09-16 13:43:53 -0400

    Recompile

--- makedist.sh
+++ makedist.sh
@@ -6,6 +6,7 @@ latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-li
 latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" linearalgebra-book &
 latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" linearalgebra-solutions &
 latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" linearalgebra-instructor &
+latexmk -lualatex -latexoption="-interaction nonstopmode -halt-on-error -file-line-error" linearalgebra-slides &
 
 # We process all the latex files in parallel and wait for them to finish
 wait $(jobs -p)
